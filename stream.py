425cbd18fac55c31844410672282696d3b203143
Mark <104472948+markysie@users.noreply.github.com>
2024-02-02 15:47:59 +0900

    Update stream.py

--- stream.py
+++ stream.py
@@ -5,3 +5,9 @@ import numpy as np
 
 st.title("제주도 관광지추천 시스템")
 st.write("""테스트 중입니다.""")
+
+if st.button('Submit'):
+    st.write('Submitted!')
+
+if st.button('Confirm'):
+    st.write('Confirmed!')
