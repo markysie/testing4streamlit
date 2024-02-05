48affb1cb8e3c84c5f68af432f86f0fff704d940
Mark <104472948+markysie@users.noreply.github.com>
2024-02-05 10:32:04 +0900

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,12 +1,12 @@
-# import streamlit as st
-# import numpy as np
-# import pandas as pd
-# from sklearn.compose import ColumnTransformer
-# from sklearn.pipeline import Pipeline
-# from sklearn.preprocessing import MinMaxScaler
-# from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.model_selection import GridSearchCV, train_test_split
-# from sklearn.metrics import accuracy_score
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.metrics import accuracy_score
 
  st.title("제주도 관광지추천")
 
